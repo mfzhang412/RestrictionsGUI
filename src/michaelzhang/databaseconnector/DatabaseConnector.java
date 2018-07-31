325527ccd5d20b83bb703b5d5d6d232f7e46e0a9
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-07-30 23:45:13 -0500

    deleted extraneous

--- src/michaelzhang/databaseconnector/DatabaseConnector.java
+++
@@ -1,7 +0,0 @@
-package michaelzhang.databaseconnector;
-/*
- * Handles the process of connecting to the database.
- */
-public class DatabaseConnector {
-	
-}
--- src/michaelzhang/errorhandler/ErrorHandler.java
+++
@@ -1,7 +0,0 @@
-package michaelzhang.errorhandler;
-/*
- * handles errors that may arise. Print why the error is wrong or print the stack trace.
- */
-public class ErrorHandler {
-
-}
